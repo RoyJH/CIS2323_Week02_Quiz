3591bdba53d3b5c406d8f26931110b62b194dd7a
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 17:59:38 -0500

    Initial commit

---
+++ Message.java
@@ -0,0 +1,6 @@
+public class Message{
+	public static void main(String[] args){
+		System.out.println("My name is Roy J Harris III, but I go by Trey.");
+		System.out.println("This week I learned that strings are a lot more simple to handle in Java than in C++.");
+	}
+}
